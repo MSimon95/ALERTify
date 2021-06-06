ca76dd7befaa55f1212997ebbebe2be2f05e346b
github username <github email address>
2021-06-06 14:55:03 -0400

    add profile

--- alertify/src/components/Alert.js
+++ alertify/src/components/Alert.js
@@ -89,7 +89,6 @@ function Alert(){
 
     return (
         <div>
-       
          {alertArr}
          <AlertUpdateForm
         alert={selectedAlert}
--- alertify/src/components/Profile.js
+++ alertify/src/components/Profile.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+
+function Profile({}) {
+    const [profileForm, setProfileForm]= useState({username: "", password_digest: "", bio: "", image: ""})
+ 
+    function updateProfileForm(event){
+        const updatedProfileFormState= {...profileForm}
+        updatedProfileFormState[event.target.name]=event.target.value
+        setProfileForm(updatedProfileFormState)
+    }
+
+  function handleSubmit(e){
+
+    e.preventDefault()
+    console.log(profileForm)
+    fetch(`http://localhost:3000/users`,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(profileForm)
+    })
+    .then(response  => response.json())
+      .then(newProfileData => 
+      {
+        const newUsers = [...allUsers, newProfileData]
+        setAllAlerts(newUsers) 
+    })
+    }
 
 
-function Profile({selectedUser}){
-
-
-    return(
-        <div>
-
-        </div>
-    )
+  return (
+    <form className="new-profile-form" onSubmit={handleSubmit}>
+      <input className="input" placeholder="event" 
+            name="event" 
+            value={profileForm.event}    
+            onChange={updateProfileForm} />
+      <input className="input" placeholder="image" 
+            name= "image" 
+            value={profileForm.image} 
+            onChange={updateProfileForm} />
+      <input className="input" placeholder="info" 
+            name= "info" 
+            value={alertForm.info} 
+            onChange={updateAlertForm} /> 
+      <input className="input" placeholder="date" 
+            name= "date" 
+            value={alertForm.date} 
+            onChange={updateAlertForm} />
+      <input className="input" placeholder="time" 
+            name= "time" 
+            value={alertForm.time} 
+            onChange={updateAlertForm} />          
+      <input className="profile-button" type="submit" 
+            value="Make This profile" />
+    </form>
+  );
 }
 
 export default Profile;
