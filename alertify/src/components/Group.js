36fe1bc58861cea411ccd269f598eb32fa4cd0dc
github username <github email address>
2021-06-13 16:01:46 -0400

    html add

--- alertify/src/components/Group.js
+++ alertify/src/components/Group.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 
 function Group(){
     const [groups, setGroups] = useState([]);
+
     const API_GROUP = "http://localhost:3000/groups"
 
     useEffect(()=>{
@@ -13,12 +14,9 @@ function Group(){
       const groupArr = groups.map((group)=> (
         <ol key={group.id}>
             <p className="group-container" id={group.id}>
-                <img src={group.image} alt={group.event}></img>
-                {group.event}
-                <li>{group.info}</li>
-                <li>{group.date}</li>
-                <li>{group.time}</li>
-                <li>{group.place}</li>
+
+                <li>{group.description}</li>
+            
                 </p>
         </ol>
       ))
