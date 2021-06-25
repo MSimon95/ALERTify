87e42f2e201840041bc50e34785da543504802b9
github username <github email address>
2021-06-25 05:00:17 -0400

    gif for about page

--- alertify/src/components/About.js
+++ alertify/src/components/About.js
@@ -4,6 +4,7 @@ function About(){
 
     return (
         <div>
+            <img src='https://media0.giphy.com/media/6IPNUgkpCsDRK/200.gif' alt="About"></img>
             <h1>Do you always forget events that you don't usually keep track?</h1>
             <h2>The Alertify is for people to remember and store everything that they need to be reminded of</h2>
             <h2>You will get a text of every detail that is important for you to remember like photo, date, place, time, and ofcourse the name of the event you need to remember. </h2>
