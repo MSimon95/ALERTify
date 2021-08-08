944e11a97cd703a5a0a69bc1cd8a36fe7e7a871e
github username <github email address>
2021-08-08 11:59:33 -0400

    alert

--- alertify/src/components/Alert.js
+++ alertify/src/components/Alert.js
@@ -77,7 +77,8 @@ function Alert({selectedUser, setSelectedUser}){
                <em><h2>{alert.event}</h2></em> 
                 <p>Info:  {alert.info}</p>
                 <p>Date: {alert.date}</p>
-
+                <p>Time:{alert.time}</p>
+                <p>Location: {alert.place}</p>
                 <button className="button2" onClick={() => handleAlert(alert)}>
         Send Alerts 
         </button>
