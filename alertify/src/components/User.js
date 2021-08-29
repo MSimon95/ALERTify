3a15adae30d852014d95d8be5d79339d8c7a8e3c
github username <github email address>
2021-08-29 13:30:57 -0400

    Delete button profile

--- alertify/src/components/User.js
+++ alertify/src/components/User.js
@@ -32,7 +32,7 @@ function User(){
                 <h1>{user.username}</h1>
                 <p>{user.bio}</p>  
                 <em>{user.phone}</em>        
-        <button type="button" className="btn btn-primary" onClick={() => handleClick(user)}>
+        <button type="button" className="btn-primary" onClick={() => handleClick(user)}>
           Choose Recepient
         </button>
         <button className="button-2" onClick={() =>handleDelete(user)} >🗑</button>
