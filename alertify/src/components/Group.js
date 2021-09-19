a1b43518d731af701e59eccd53cd8f0258386bc1
github username <github email address>
2021-09-19 14:06:39 -0400

    group

--- alertify/src/components/Group.js
+++ alertify/src/components/Group.js
@@ -11,12 +11,22 @@ function Group(){
           .then((res) => res.json())
           .then((data) => setGroups(data))
       }, [])
+      function handleDelete (groupObj){
+        fetch(`http://localhost:3000/users/${groupObj.id}`, {
+            method: 'DELETE'
+              })
+              .then(res => res.text())
+              .then(res => console.log(res))
+        const updatedUsers = users.filter((user) => user.id !== userObj.id);
+        setUsers(updatedUsers);
+    }
       
       const groupArr = groups.map((group)=> (
         <ol key={group.id}>
             <p className="group-container" id={group.id}>
                 <h2>{group.description}</h2>
                 <h3>{group.user.username}</h3>
+                <button className="button-2" onClick={() =>handleDelete()} >🗑</button>
                 <br></br>   
                 </p>
         </ol>
